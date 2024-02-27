8f038304f870e9741b8b12fa4545b0d887a7090b
jmorga1563 <jmorga1563@my.sullivan.edu>
2024-02-27 12:43:10 -0500

    Created initial form GUI.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace CSC240_GameProject_jrm
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
